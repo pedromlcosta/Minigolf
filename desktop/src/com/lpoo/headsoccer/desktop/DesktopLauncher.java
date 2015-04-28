5d3bd37309d49de8899d62bad1eef0e9d77b3160
José Rebelo <soaresrebelo@gmail.com>
2015-04-28 17:24:22 +0100

    First commit

---
+++ desktop/src/com/lpoo/headsoccer/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.lpoo.headsoccer.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.lpoo.headsoccer.HeadSoccer;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new HeadSoccer(), config);
+	}
+}
