3e61a10651f2fe4ee89dd3a992a937e45890be3c
Ncf16 <joaofilipe26@gmail.com>
2015-04-28 17:54:02 +0100

    Testar
    Hope it owrks

--- desktop/src/com/lpoo/MiniGolf/desktop/DesktopLauncher.java
+++ desktop/src/com/lpoo/MiniGolf/desktop/DesktopLauncher.java
@@ -1,12 +1,12 @@
-package com.lpoo.headsoccer.desktop;
+package com.lpoo.MiniGolf.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.lpoo.headsoccer.HeadSoccer;
+import com.lpoo.MiniGolf.MiniGolf;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new HeadSoccer(), config);
+		new LwjglApplication(new MiniGolf(), config);
 	}
 }
