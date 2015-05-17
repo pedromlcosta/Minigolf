f9135d49e189cd3696ac49136edbd1e74e306656
Ncf16 <joaofilipe26@gmail.com>
2015-05-17 14:38:51 +0100

    mistake

--- core/src/com/lpoo/MiniGolf/logic/MiniGolf.java
+++ core/src/com/lpoo/MiniGolf/logic/MiniGolf.java
@@ -3,6 +3,8 @@ package com.lpoo.MiniGolf.logic;
 import java.util.ArrayList;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -46,46 +48,18 @@ public class MiniGolf extends Game {
 	public void render() {
 		W = new World(new Vector2(0, -10), true);
 		 
-		//
-		// Gdx.gl.glClearColor(1, 0, 0, 1);
-		// Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		// batch.begin();
-		// int xi = 0;
-		// int yi = 0;
-		// for (int i = 0; i < 15; i++) {
-		// batch.draw(img.getTexture(), xi, yi, 20, 20);
-		// xi += 20;
-		// yi += 20;
-		// }
-		// batch.end();
-		// First we create a body definition
-		BodyDef bodyDef = new BodyDef();
-		// We set our body to dynamic, for something like ground which doesn't
-		// move we would set it to StaticBody
-		bodyDef.type = BodyType.DynamicBody;
-		// Set our body's starting position in the world
-		bodyDef.position.set(100, 300);
-
-		// Create our body in the world using our body definition
-		Body body = W.createBody(bodyDef);
-
-		// Create a circle shape and set its radius to 6
-		CircleShape circle = new CircleShape();
-		circle.setRadius(6f);
-
-		// Create a fixture definition to apply our shape to
-		FixtureDef fixtureDef = new FixtureDef();
-		fixtureDef.shape = circle;
-		fixtureDef.density = 0.5f;
-		fixtureDef.friction = 0.4f;
-		fixtureDef.restitution = 0.6f; // Make it bounce a little bit
-
-		// Create our fixture and attach it to the body
-		Fixture fixture = body.createFixture(fixtureDef);
-
-		// Remember to dispose of any shapes after you're done with them!
-		// BodyDef and FixtureDef don't need disposing, but shapes do.
-		circle.dispose();
+		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		batch.begin();
+		int xi = 0;
+		int yi = 0;
+		for (int i = 0; i < 15; i++) {
+			batch.draw(img.getTexture(), xi, yi, 20, 20);
+			xi += 20;
+			yi += 20;
+		}
+		batch.end();
+	 
 	}
 
 	public SpriteBatch getBatch() {
