b4c4d6fa43e8d6d30fdb077fa1611ed3f6fe7fe8
pedromlcosta <up201303973@fe.up.pt>
2015-06-08 10:58:04 +0100

    Kryonet stuff

---
+++ core/src/com/lpoo/MiniGolf/screens/Comando.java
@@ -0,0 +1,38 @@
+package com.lpoo.MiniGolf.screens;
+
+import java.io.Serializable;
+
+public class Comando implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	int action;
+	
+	int x, y;
+	
+	public Comando() {
+		
+	}
+	
+	public Comando(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+	
+}
