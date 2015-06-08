707001bef433c4caad7dd152a109c2407efee9a9
Ncf16 <Filipe-PC@Filipe.home>
2015-06-08 14:50:39 +0100

    auto

--- core/src/com/lpoo/MiniGolf/logic/MiniGolf.java
+++ core/src/com/lpoo/MiniGolf/logic/MiniGolf.java
@@ -20,38 +20,91 @@ import com.badlogic.gdx.utils.viewport.Viewport;
 import com.lpoo.MiniGolf.data.GameIO;
 import com.lpoo.MiniGolf.screens.LoadScreen;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class MiniGolf.
+ */
 public class MiniGolf extends Game {
 
+	/** The batch. */
 	public static SpriteBatch batch;
+	
+	/** The cam. */
 	public static OrthographicCamera cam;
+	
+	/** The viewport. */
 	public static Viewport viewport;
+	
+	/** The w. */
 	private static World W;
+	
+	/** The load save. */
 	private GameIO loadSave;
+	
+	/** The nr players. */
 	private int nrPlayers = 3;
+	
+	/** The selected courses. */
 	private ArrayList<Course> selectedCourses = new ArrayList<Course>();
+	
+	/** The all courses. */
 	private static ArrayList<Course> allCourses = new ArrayList<Course>();
 
+	/** The nr courses. */
 	private static int nrCourses = 1;
+	
+	/** The Constant MAX_PLAYERS. */
 	public static final int MAX_PLAYERS = 4;
 
+	/** The random course. */
 	private static boolean randomCourse;
+	
+	/** The end point. */
 	private Vector2 endPoint;
+	
+	/** The start point. */
 	private Vector2 startPoint;
+	
+	/** The tacadas max. */
 	private int tacadasMax = 3;
+	
+	/** The tempo max. */
 	private int tempoMax = 10;
+	
+	/** The course height. */
 	private int courseHeight;
+	
+	/** The course width. */
 	private int courseWidth;
 
+	/** The Constant BOX_TO_WORLD. */
 	public static final float BOX_TO_WORLD = 100f;
+	
+	/** The Constant TITLE. */
 	public static final String TITLE = "Game Project";
+	
+	/** The width. */
 	public static float WIDTH = 1920;
+	
+	/** The height. */
 	public static float HEIGHT = 1080;
+	
+	/** The Constant BUTTON_WIDTH. */
 	public  static final float BUTTON_WIDTH = 200f;
+	
+	/** The Constant BUTTON_HEIGHT. */
 	public static final float BUTTON_HEIGHT = 50f;
+	
+	/**
+	 * Instantiates a new mini golf.
+	 */
 	public MiniGolf() {
 
 	}
 
+	/* (non-Javadoc)
+	 * @see com.badlogic.gdx.ApplicationListener#create()
+	 */
 	public void create() {
 
 		loadSave = new GameIO();
@@ -198,46 +251,104 @@ public class MiniGolf extends Game {
 
 	// TODO PASSAR ISTO PARA O SHOW DO GAMESCREEN
 
+	/**
+	 * Gets the w.
+	 *
+	 * @return the w
+	 */
 	public static World getW() {
 		return W;
 	}
 
+	/**
+	 * Sets the w.
+	 *
+	 * @param w the new w
+	 */
 	public static void setW(World w) {
 		W = w;
 	}
 
+	/**
+	 * Gets the batch.
+	 *
+	 * @return the batch
+	 */
 	public static SpriteBatch getBatch() {
 		return batch;
 	}
 
+	/**
+	 * Sets the batch.
+	 *
+	 * @param batch the new batch
+	 */
 	public static void setBatch(SpriteBatch batch) {
 		MiniGolf.batch = batch;
 	}
 
+	/**
+	 * Gets the cam.
+	 *
+	 * @return the cam
+	 */
 	public static OrthographicCamera getCam() {
 		return cam;
 	}
 
+	/**
+	 * Sets the cam.
+	 *
+	 * @param cam the new cam
+	 */
 	public static void setCam(OrthographicCamera cam) {
 		MiniGolf.cam = cam;
 	}
 
+	/**
+	 * Gets the course height.
+	 *
+	 * @return the course height
+	 */
 	public int getCourseHeight() {
 		return courseHeight;
 	}
 
+	/**
+	 * Sets the course height.
+	 *
+	 * @param courseHeight the new course height
+	 */
 	public void setCourseHeight(int courseHeight) {
 		this.courseHeight = courseHeight;
 	}
 
+	/**
+	 * Gets the course width.
+	 *
+	 * @return the course width
+	 */
 	public int getCourseWidth() {
 		return courseWidth;
 	}
 
+	/**
+	 * Sets the course width.
+	 *
+	 * @param courseWidth the new course width
+	 */
 	public void setCourseWidth(int courseWidth) {
 		this.courseWidth = courseWidth;
 	}
 
+	/**
+	 * Creates the edge.
+	 *
+	 * @param x1 the x1
+	 * @param y1 the y1
+	 * @param x2 the x2
+	 * @param y2 the y2
+	 */
 	void createEdge(float x1, float y1, float x2, float y2) {
 		BodyDef bodyDef = new BodyDef();
 		bodyDef.type = BodyDef.BodyType.StaticBody;
@@ -251,58 +362,126 @@ public class MiniGolf extends Game {
 		fixt.setFriction(0.5f);
 	}
 
+	/**
+	 * Gets the end point.
+	 *
+	 * @return the end point
+	 */
 	public Vector2 getEndPoint() {
 		return endPoint;
 	}
 
+	/**
+	 * Sets the end point.
+	 *
+	 * @param endPoint the new end point
+	 */
 	public void setEndPoint(Vector2 endPoint) {
 		this.endPoint = endPoint;
 	}
 
+	/**
+	 * Gets the start point.
+	 *
+	 * @return the start point
+	 */
 	public Vector2 getStartPoint() {
 		return startPoint;
 	}
 
+	/**
+	 * Sets the start point.
+	 *
+	 * @param startPoint the new start point
+	 */
 	public void setStartPoint(Vector2 startPoint) {
 		this.startPoint = startPoint;
 	}
 
+	/**
+	 * Gets the tacadas max.
+	 *
+	 * @return the tacadas max
+	 */
 	public int getTacadasMax() {
 		return tacadasMax;
 	}
 
+	/**
+	 * Sets the tacadas max.
+	 *
+	 * @param tacadasMax the new tacadas max
+	 */
 	public void setTacadasMax(int tacadasMax) {
 		this.tacadasMax = tacadasMax;
 	}
 
+	/**
+	 * Gets the tempo max.
+	 *
+	 * @return the tempo max
+	 */
 	public int getTempoMax() {
 		return tempoMax;
 	}
 
+	/**
+	 * Sets the tempo max.
+	 *
+	 * @param tempoMax the new tempo max
+	 */
 	public void setTempoMax(int tempoMax) {
 		this.tempoMax = tempoMax;
 	}
 
+	/**
+	 * Gets the height.
+	 *
+	 * @return the height
+	 */
 	public static float getHeight() {
 		return HEIGHT;
 	}
 
+	/**
+	 * Sets the height.
+	 *
+	 * @param height the new height
+	 */
 	public static void setHeight(int height) {
 		HEIGHT = height;
 	}
 
+	/**
+	 * Gets the width.
+	 *
+	 * @return the width
+	 */
 	public static float getWidth() {
 		return WIDTH;
 	}
 
+	/**
+	 * Sets the width.
+	 *
+	 * @param width the new width
+	 */
 	public static void setWidth(int width) {
 		WIDTH = width;
 	}
 
+	/**
+	 * Checks if is random course.
+	 *
+	 * @return true, if is random course
+	 */
 	public static boolean isRandomCourse() {
 		return randomCourse;
 	}
 
+	/**
+	 * Random courses.
+	 */
 	public void randomCourses() {
  
 		if(allCourses.isEmpty())
@@ -320,54 +499,119 @@ public class MiniGolf extends Game {
 		}
 	}
 
+	/**
+	 * Sets the random course.
+	 *
+	 * @param randomCourse the new random course
+	 */
 	public static void setRandomCourse(boolean randomCourse) {
 		MiniGolf.randomCourse = randomCourse;
 	}
 
+	/**
+	 * Gets the nr courses.
+	 *
+	 * @return the nr courses
+	 */
 	public static int getNrCourses() {
 		return nrCourses;
 	}
 
+	/**
+	 * Sets the nr courses.
+	 *
+	 * @param nrCourses the new nr courses
+	 */
 	public static void setNrCourses(int nrCourses) {
 		MiniGolf.nrCourses = nrCourses;
 	}
 
+	/**
+	 * Gets the nr players.
+	 *
+	 * @return the nr players
+	 */
 	public int getNrPlayers() {
 		return nrPlayers;
 	}
 
+	/**
+	 * Sets the nr players.
+	 *
+	 * @param nrPlayers the new nr players
+	 */
 	public void setNrPlayers(int nrPlayers) {
 		this.nrPlayers = nrPlayers;
 	}
 
+	/**
+	 * Gets the selected courses.
+	 *
+	 * @return the selected courses
+	 */
 	public ArrayList<Course> getSelectedCourses() {
 		return selectedCourses;
 	}
 
+	/**
+	 * Sets the selected courses.
+	 *
+	 * @param selectedCourses the new selected courses
+	 */
 	public void setSelectedCourses(ArrayList<Course> selectedCourses) {
 		this.selectedCourses = selectedCourses;
 	}
 
+	/**
+	 * Adds the to selected courses.
+	 *
+	 * @param course the course
+	 */
 	public void addToSelectedCourses(Course course) {
 		selectedCourses.add(course);
 	}
 
+	/**
+	 * Gets the all courses.
+	 *
+	 * @return the all courses
+	 */
 	public static ArrayList<Course> getAllCourses() {
 		return allCourses;
 	}
 
+	/**
+	 * Sets the all courses.
+	 *
+	 * @param allCourses the new all courses
+	 */
 	public void setAllCourses(ArrayList<Course> allCourses) {
 		MiniGolf.allCourses = allCourses;
 	}
 
+	/**
+	 * Adds the to all courses.
+	 *
+	 * @param course the course
+	 */
 	public void addToAllCourses(Course course) {
 		allCourses.add(course);
 	}
 
+	/**
+	 * Gets the load save.
+	 *
+	 * @return the load save
+	 */
 	public GameIO getLoadSave() {
 		return loadSave;
 	}
 
+	/**
+	 * Sets the load save.
+	 *
+	 * @param loadSave the new load save
+	 */
 	public void setLoadSave(GameIO loadSave) {
 		this.loadSave = loadSave;
 	}
